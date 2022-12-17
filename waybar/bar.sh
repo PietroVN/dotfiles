0bc4fecfe807ba6530328beeca61c8c21dd569ca
Waltz <wa1t5@proton.me>
2022-12-17 16:32:59 -0300

    update dotfiles

--- waybar/bar.sh
+++ waybar/bar.sh
@@ -5,7 +5,7 @@
 ###################################
 
 # Paths
-song_name_path="$XDG_RUNTIME_DIR/name"
+song_name_path="$XDG_RUNTIME_DIR/music-name"
 song_pause_path="$XDG_RUNTIME_DIR/music-paused"
 mpv_socket_path="$XDG_RUNTIME_DIR/music-socket"
 
@@ -48,15 +48,18 @@ music() {
 
 		# Roll size (the max characters that will be visible at the same time)
 		#roll_size="30"
-		rool_size="$title_size"
+		
+		#
+		roll_size="$title_size"
+		roll_end="$title_size"
 
 		# Initial values
-		[[ "$roll_end" -ge "$title_size" || -z "$roll_start" ]] && roll_start="1"
-		[[ "$roll_end" -ge "$title_size" || -z "$roll_end" ]] && roll_end="$roll_size"
+		#[[ "$roll_end" -ge "$title_size" || -z "$roll_start" ]] && roll_start="1"
+		#[[ "$roll_end" -ge "$title_size" || -z "$roll_end" ]] && roll_end="$roll_size"
 
 		# Song name var (if paused song name will be exhibited with exclamation  prefix)
-		[[ $(cat "$song_pause_path") == "no" || $(cat "$song_pause_path" | wc -l) ]] && music=" $(cut -b "$roll_start"-"$roll_end" "$song_name_path")"
-		[[ $(cat "$song_pause_path") == "yes" ]] && music=" $(cut -b "$roll_start"-"$roll_end" "$song_name_path")"
+		[[ -f "$song_pause_path" ]] || music=" $(cut -b "$roll_start"-"$roll_end" "$song_name_path")"
+		[[ -f "$song_pause_path" ]] && music=" $(cut -b "$roll_start"-"$roll_end" "$song_name_path")"
 
 		# Make the roll effect
 		#roll_start=$((roll_start + 1))
